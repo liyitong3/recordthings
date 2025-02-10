1e7d334ce739bb86560d52752b919e623fa45362
李一桐 <liyitong@email.cn>
2025-02-10 13:16:43 +0800

    Update mysql_util.py
    数据库连接信息

--- mysql_util.py
+++ mysql_util.py
@@ -7,8 +7,8 @@ class MysqlUtil():
         '''
             初始化方法，连接数据库
         '''
-        host = '127.0.0.1'    # 主机名
-        user = 'root'         # 数据库用户名
+        host = '127.0.0.1'      # 主机名
+        user = 'root'           # 数据库用户名
         password = '123456'     # 数据库密码
         database = 'memory_things_system' # 数据库名称
         self.db = pymysql.connect(host=host,user=user,password=password,db=database) # 建立连接
